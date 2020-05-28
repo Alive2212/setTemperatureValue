01be12057f64119aeb8e9516c826846c39eb2c49
Babak Nodoust <alive2212@gmail.com>
2020-05-28 20:11:11 +0430

    add driver to implement later

--- main.py
+++ main.py
@@ -6,12 +6,18 @@ defaultTemperatureValue = json.load(jsonFile)
 
 
 def main():
-    print(getTemperatureValue())
+    set_temperature_value_driver(get_temperature_value())
 
-def getTemperatureValue():
+
+def get_temperature_value():
     # monday is 0 and sunday is 7
     toDay = datetime.datetime.today()
     return defaultTemperatureValue[str(toDay.isocalendar()[2])][str(toDay.hour)]
+
+def set_temperature_value_driver(temperature_controller_value):
+    # TODO here should implement something to communicate with IOT Devices
+    print(temperature_controller_value)
+
 
 if __name__ == '__main__':
     main()
